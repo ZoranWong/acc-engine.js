a6eb8401789bc81e4b066c78f41390735b494b80
ZoranWong <904110231@qq.com>
2020-05-14 22:42:42 +0800

    change command

--- src/io/http/Adapter.js
+++ src/io/http/Adapter.js
@@ -6,30 +6,27 @@ export default class Adapter {
     constructor(app) {
         this.#app = app;
     }
-    async request() {
-        return false;
+    async get(url, querys = {}) {
+        this.#url = url;
+        this.#data = querys;
+        this.#method = 'GET';
     }
-    // async get(url, querys = {}) {
-    //     this.#url = url;
-    //     this.#data = querys;
-    //     this.#method = 'GET';
-    // }
-    //
-    // async post(url, data = {}) {
-    //     this.#url = url;
-    //     this.#data = data;
-    //     this.#method = 'POST';
-    // }
-    //
-    // async put(url, data = {}) {
-    //     this.#url = url;
-    //     this.#data = data;
-    //     this.#method = 'PUT';
-    // }
-    //
-    // async del(url, data = {}) {
-    //     this.#url = url;
-    //     this.#data = data;
-    //     this.#method = 'DELETE';
-    // }
+
+    async post(url, data = {}) {
+        this.#url = url;
+        this.#data = data;
+        this.#method = 'POST';
+    }
+
+    async put(url, data = {}) {
+        this.#url = url;
+        this.#data = data;
+        this.#method = 'PUT';
+    }
+
+    async del(url, data = {}) {
+        this.#url = url;
+        this.#data = data;
+        this.#method = 'DELETE';
+    }
 }
