ec837d1053795ca7f2f01d6dd8473881ae617c4b
ZoranWong <904110231@qq.com>
2020-05-20 11:58:02 +0800

    model driver fix

--- src/models/DBDriver.js
+++ src/models/DBDriver.js
@@ -1,5 +1,21 @@
+import {
+    each
+} from 'underscore';
 export default class DBDriver {
-    constructor(store) {
+    #models = {};
+    #store = null;
+    constructor(models, store) {
+        this.#store = store;
+        each(models, (model, key) => {
+            this.#models[key] = this.model(model);
+        });
+    }
 
+    model(model) {
+        return model;
+    }
+
+    addModel(name, model) {
+        this.#models[name] = model;
     }
 }
