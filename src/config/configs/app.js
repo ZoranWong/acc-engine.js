5c5f1afe6ae7148509b8c78ce3b6865100770f8b
ZoranWong <904110231@qq.com>
2020-05-21 16:04:47 +0800

    remove some unused service

--- src/config/configs/app.js
+++ src/config/configs/app.js
@@ -1,7 +1,5 @@
 import WorkerServiceProvider from '../../worker/WorkerServiceProvider';
 import HttpServiceProvider from '../../io/http/HttpServiceProvider';
-import SocketServiceProvider from '../../io/socket/SocketServiceProvider';
-import LogsServiceProvider from '../../logs/LogsServiceProvider';
 import CommandServiceProvider from '../../command/CommandServiceProvider';
 import EventServiceProvider from '../../events/EventServiceProvider';
 import ModelServiceProvider from '../../models/ModelServiceProvider';
