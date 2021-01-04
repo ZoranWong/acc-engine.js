ad63741e92894530a68322326430270a2f0ac611
ZoranWong <904110231@qq.com>
2021-01-04 17:07:04 +0800

    升级

--- src/worker/WorkerServiceProvider.js
+++ src/worker/WorkerServiceProvider.js
@@ -1,8 +1,9 @@
 import ServiceProvider from '../constracts/ServiceProvider';
-import WorkerManager from './WorkerManager';
+// import WorkerManager from './WorkerManager';
 export default class WorkerServiceProvider extends ServiceProvider {
     register() {
         if(typeof eval !== 'undefined') {
+            const WorkerManager = require('./WorkerManager').default;
             this.app.singleton('workerManager', (app) => {
                 return new WorkerManager(app)
             });
