1a29f2c153ead8a7148d8d91bdabe6213216ad0d
ZoranWong <904110231@qq.com>
2021-02-01 09:17:27 +0800

    model优化

--- src/models/Model.js
+++ src/models/Model.js
@@ -1,41 +1,38 @@
-import {extend, forEach, isFunction} from "underscore";
+import {extend, forEach, isFunction, isEmpty, where} from "underscore";
 import Application, {caseKeyName} from "..";
 
 export default class Model {
     cacheAttributes = ['*'];
     savingCache = false;
     app = null;
-    cacheKey = 'model_';
+    cacheKey = '';
     needCache = false;
-    excludeKeys = ['excludeKeys', 'savingCache', 'needCache', 'namespace', 'cacheAttributes', 'app', 'cacheKey', 'state', 'getters', 'actions', 'mutations'];
-    namespace = '';
+    excludeCacheKeys = ['excludeCacheKeys', 'savingCache', 'needCache', 'cacheAttributes', 'app', 'cacheKey', 'state', 'getters', 'actions', 'mutations'];
 
-    constructor (options = {namespace: ''}) {
+    constructor (options = {cacheKey: ''}) {
         this.app = Application.getInstance();
+        this.cacheKey += options.cacheKey || '';
     }
 
-    static instance (app, namespace) {
-        let instance = new this(app, namespace);
+    static instance (cacheKey = '') {
+        let instance = new this({cacheKey});
         instance.initial();
         return instance;
     }
 
     resetModelFromCache () {
-        if (this.needCache && this.namespace && this.app['cache']) {
-            let cachedData = this.app['cache'].get(this.cacheKey + this.namespace);
+        if (this.needCache && this.app['cache']) {
+            let cachedData = this.app['cache'].get(this.cacheKey);
             let reset = (cachedData) => {
                 if (cachedData) {
-                    if (this.cacheAttributes.length === 1 && this.cacheAttributes[0] === '*') {
-                        forEach(cachedData, (item, key) => {
-                            this.set(key, item);
-                        });
-                    } else {
-                        forEach(this.cacheAttributes, (key) => {
-                            if (cachedData[key]) {
-                                this.set(key, cachedData[key]);
-                            }
+                    if (this.cacheAttributes.length !== 1 || this.cacheAttributes.includes('*')) {
+                        cachedData = where(cachedData, (item, key) => {
+                            return this.cacheAttributes.includes(key);
                         });
                     }
+                    forEach(cachedData, (item, key) => {
+                        this.set(key, item);
+                    });
                 }
             }
             if (cachedData instanceof Promise) {
@@ -49,30 +46,31 @@ export default class Model {
     }
 
     setCache () {
-        if (this.needCache && this.namespace && this.app['cache']) {
-            let cacheData = {};
-            if (this.cacheAttributes.length === 1 && this.cacheAttributes[0] === '*') {
-                forEach(this, (item, key) => {
-                    if (this.excludeKeys.indexOf(key) === -1 && !isFunction(this[key])) {
-                        cacheData[key] = item;
-                    }
-                })
-            } else {
-                forEach(this.cacheAttributes, (key) => {
-                    cacheData[key] = this[key];
-                });
-            }
-            this.app['cache'].set(this.cacheKey + this.namespace, cacheData);
+        if (this.needCache && this.app['cache'] && !this.savingCache) {
+            this.savingCache = true;
+            setTimeout(async () => {
+                let cacheData = {};
+                if (this.cacheAttributes.length === 1 && this.cacheAttributes[0] === '*') {
+                    forEach(this, (item, key) => {
+                        if (this.excludeCacheKeys.indexOf(key) === -1 && !isFunction(this[key])) {
+                            cacheData[key] = item;
+                        }
+                    })
+                } else {
+                    forEach(this.cacheAttributes, (key) => {
+                        cacheData[key] = this[key];
+                    });
+                }
+                await this.app['cache'].set(this.cacheKey, cacheData);
+                this.savingCache = false;
+            }, 200);
         }
     }
 
-    initial (options) {
-        if (!options['namespace']) {
-            options['namespace'] = caseKeyName(this.constructor.name);
-        }
-        this.namespace = options['namespace'];
+    initial (options = {}) {
         this.resetModelFromCache();
-        this.setModel(options);
+        if (!isEmpty(options))
+            this.setModel(options);
         return this;
     }
 
@@ -83,7 +81,7 @@ export default class Model {
         return value;
     }
 
-    setModel (data) {
+    setModel (data = {}) {
         for (const key in data) {
             const camelKey = caseKeyName(key);
             if (typeof this[camelKey] !== 'undefined') {
